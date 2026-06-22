70a8bcda306057f7cec6cc6b16183c21f99e1706
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-22 09:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (6.93%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.c
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.c
@@ -7,10 +7,15 @@ char* reverseVowels(char* s) {
     char *result = malloc(length + 1);
     strcpy(result, s);
 
+    // O(1) vowel check (i used strchr which was inefficient)
+    bool isVowel[128] = {0};
+    isVowel['a'] = isVowel['e'] = isVowel['i'] = isVowel['o'] = isVowel['u'] = true;
+    isVowel['A'] = isVowel['E'] = isVowel['I'] = isVowel['O'] = isVowel['U'] = true;
+
     int l = 0, r = length - 1;
     while (l < r) {
-        while (l < r && !strchr("AEIOUaeiou", result[l])) l++;
-        while (l < r && !strchr("AEIOUaeiou", result[r])) r--;
+        while (l < r && !isVowel[(unsigned char) result[l]]) l++;
+        while (l < r && !isVowel[(unsigned char) result[r]]) r--;
         if (l < r) {
             char tmp = result[l];
             result[l++] = result[r];
