641ce432de13c804c6dc11103fe2c5da67289006
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-25 10:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (42.37%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.c
@@ -0,0 +1,14 @@
+bool increasingTriplet(int* nums, int numsSize) {
+    int first = INT_MAX;
+    int second = INT_MAX;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] <= first)
+            first = nums[i];
+        else if (nums[i] <= second)
+            second = nums[i];
+        else
+            return true; // first < second < nums[i]
+    }
+    return false;
+}
