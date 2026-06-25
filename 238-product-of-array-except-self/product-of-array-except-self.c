365e6810e23bde930df66fc5a40004ffa55c1152
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-25 08:53:32 +0530

    Time: 3 ms (29.49%) | Memory: 30.8 MB (97.59%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -2,15 +2,17 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
+// Approach 1- Prefix & Suffix variables
+// Approach 2- Suffix only
+
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     int* answer = (int*)malloc(numsSize * sizeof(int));
     *returnSize = numsSize;
 
     // Pass 1: prefix products
-    int prefix = 1;
-    for (int i = 0; i < numsSize; i++) {
-        answer[i] = prefix;
-        prefix *= nums[i];
+    answer[0] = 1;
+    for (int i = 1; i < numsSize; i++) {
+        answer[i] = answer[i-1] * nums[i-1];
     }
 
     // Pass 2: multiply suffix products
