576cf89df60d3fe53db8ee39a5af498c70154509
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-26 08:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (73.47%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.c
+++ 1894-merge-strings-alternately/merge-strings-alternately.c
@@ -1,8 +1,9 @@
 char* mergeAlternately(char* word1, char* word2) {
     int idx = 0, i = 0, j = 0;
-    // malloced 256 bytes as an apt number near constraints
-    char* result = (char*)malloc(256 * sizeof(char));
     
+    int len = strlen(word1) + strlen(word2) + 1;
+    char* result = (char*)malloc(len);
+
     while (word1[i] != '\0' && word2[j] != '\0') {
         result[idx++] = word1[i++];
         result[idx++] = word2[j++];
