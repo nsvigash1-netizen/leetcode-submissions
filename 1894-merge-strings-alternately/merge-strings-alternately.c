8e090813cfeb435601b2e8d85732bac193b3ea58
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-26 08:05:27 +0530

    Time: 4 ms (32.22%) | Memory: 8.8 MB (41.43%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.c
+++ 1894-merge-strings-alternately/merge-strings-alternately.c
@@ -1,28 +1,13 @@
 #include <stdbool.h>
 
 char* mergeAlternately(char* word1, char* word2) {
-    int idx = 0;
+    int idx = 0, i = 0, j = 0;
     // malloced 256 bytes as an apt number near constraints
     char* result = (char*)malloc(256 * sizeof(char));
-    bool hasVal[2] = {true, true};
-    int i = 0;
-    while (hasVal[0] || hasVal[1]) {
-        // word1
-        if (hasVal[0]) {
-            if (word1[i] != '\0')
-                result[idx++] = word1[i];
-            else
-                hasVal[0] = false;
-        }
-        // word2
-        if (hasVal[1]) {
-            if (word2[i] != '\0')
-                result[idx++] = word2[i];
-            else
-                hasVal[1] = false;
-        }
-        // incrementing i
-        i++;
+    
+    while (word1[i] != '\0' || word2[j] != '\0') {
+        if (word1[i] != '\0') result[idx++] = word1[i++];
+        if (word2[j] != '\0') result[idx++] = word2[j++];
     }
     result[idx] = '\0';
     return result;
