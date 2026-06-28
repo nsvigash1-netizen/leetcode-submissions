b7cf5039977cd043f77fa39e2fab073dbfd4b57e
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-28 17:52:26 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (74.58%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.c
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.c
@@ -1,16 +1,23 @@
 double findMaxAverage(int* nums, int numsSize, int k) {
     double sum = 0;
+    double result = 0;
 
-    for (int j = 0; j < k; j++)
-        sum += nums[j];
+    for (int i = 0, end = numsSize - k + 1; i < end; i++) {
 
-    double result = sum;
-
-    for (int i = 1, end = numsSize - k + 1; i < end; i++) {
-        sum += nums[i + k - 1] - nums[i - 1];
-        if (sum > result)
+        if (i == 0) {
+            for (int j = 0; j < k; j++) {
+                sum += nums[j];
+            }
             result = sum;
+        } else {
+            sum -= nums[i - 1];
+            sum += nums[i + k - 1];
+
+            if (sum > result)
+                result = sum;
+        }
     }
     result /= k;
+
     return result;
 }
