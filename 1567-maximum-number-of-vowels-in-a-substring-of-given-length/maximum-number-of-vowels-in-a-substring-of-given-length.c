fc6966f20430c2cbbb1a306e19d8b4123efdaaa1
nsvigash1-netizen <nsvigash1@gmail.com>
2026-06-29 08:26:11 +0530

    Time: 3 ms (75.90%) | Memory: 10.2 MB (9.74%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.c
@@ -0,0 +1,22 @@
+int maxVowels(char* s, int k) {
+    int c = 0;
+    int maxVowel = 0;
+    for (int i = 0, end = strlen(s) - k + 1; i < end; i++) {
+        if (i == 0) {
+            for (int j = 0; j < k; j++) {
+                // bitmasking to check vowels
+                if ((0x208222 >> (s[j] & 0x1f)) & 1)
+                    c++;
+            }
+            maxVowel = c;
+        } else {
+            if ((0x208222 >> (s[i - 1] & 0x1f)) & 1)
+                c--;
+            if ((0x208222 >> (s[i + k - 1] & 0x1f)) & 1)
+                c++;
+            if (c > maxVowel)
+                maxVowel = c;
+        }
+    }
+    return maxVowel;
+}
