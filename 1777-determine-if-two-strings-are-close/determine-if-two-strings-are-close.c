27ae506f7a5b3833a4ede9b2873ff986ddfbe607
nsvigash1-netizen <nsvigash1@gmail.com>
2026-07-03 00:49:15 +0530

    Time: 7 ms (62.18%) | Memory: 13.6 MB (5.88%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.c
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.c
@@ -1,9 +1,9 @@
 #define ALPHABET 26
 #define MAXN 100005
 
-// counts the number of iterations of chars in word1 and word2
-int count1[MAXN] = {0};
-int count2[MAXN] = {0};
+// freqOfFreq1[k] / freqOfFreq2[k] = how many letters occur exactly k times
+int freqOfFreq1[MAXN] = {0};
+int freqOfFreq2[MAXN] = {0};
 
 bool closeStrings(char* word1, char* word2) {
     int len1 = strlen(word1);
@@ -12,36 +12,36 @@ bool closeStrings(char* word1, char* word2) {
     if (len1 != len2)
         return false;
 
-    memset(count1, 0, sizeof(count2));   
-    memset(count2, 0, sizeof(count2));   
+    memset(freqOfFreq1, 0, sizeof(freqOfFreq1));
+    memset(freqOfFreq2, 0, sizeof(freqOfFreq2));
 
-    int freq1[ALPHABET] = {0};
-    int freq2[ALPHABET] = {0};
+    // letterCount1[c] / letterCount2[c] = how many times letter c appears
+    int letterCount1[ALPHABET] = {0};
+    int letterCount2[ALPHABET] = {0};
 
-    for (int i = 0; i < len1; i++) {
-        freq1[word1[i] - 'a']++;
-    }
+    for (int i = 0; i < len1; i++)
+        letterCount1[word1[i] - 'a']++;
+    for (int i = 0; i < len2; i++)
+        letterCount2[word2[i] - 'a']++;
 
-    for (int i = 0; i < len2; i++) {
-        freq2[word2[i] - 'a']++;
-    }
-
-    // character sets must match
+    // both words must use exactly the same set of letters
     for (int i = 0; i < ALPHABET; i++) {
-        if ((freq1[i] == 0) != (freq2[i] == 0))
+        if ((letterCount1[i] == 0) != (letterCount2[i] == 0))
             return false;
     }
 
-    // frequency multisets must match
+    // build frequency-of-frequency: e.g. "aabbb" -> one letter w/ count 2, one w/ count 3
     for (int i = 0; i < ALPHABET; i++) {
-        count1[freq1[i]]++;
-        count2[freq2[i]]++;
+        freqOfFreq1[letterCount1[i]]++;
+        freqOfFreq2[letterCount2[i]]++;
     }
 
-
-    for (int i = 0; i < len1; i++) {
-        if (count1[i] != count2[i])
+    // the multiset of letter-frequencies must match (order doesn't matter,
+    // e.g. "aabbb" and "bbaaa" both have {count 2, count 3})
+    for (int i = 0; i <= len1; i++) {
+        if (freqOfFreq1[i] != freqOfFreq2[i])
             return false;
     }
+
     return true;
 }
